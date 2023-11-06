1e7eef760cb1e59e034812128711560b6e7b978d
sara <sara.mohebati71@gmail.com>
2023-11-06 09:14:41 +0330

    Add Cart Item

---
+++ src/components/CartItem.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
+
+const CartItem = ({ item }) => {
+  const { id, image, title, price, amount } = item;
+  return (
+    <div className="flex gap-x-4 py-2 px-6 border-b order-gray-200 
+    w-full font-light text-gray-500">
+      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+        <Link to={`/product/${id}`}>
+          <img className="max-w-[80px] " src={image} alt="" />
+        </Link>
+
+        <div className="w-full flex flex-col">
+          <div className="flex justify-between mb-2">
+            <Link
+              to={`/product/${id}`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              {title}
+            </Link>
+            <div className="text-xl cursor-pointer">
+              <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
+            </div>
+          </div>
+          <div className="flex gap-x-2 h-[36px] text-sm">
+            <div className="flex flex-1 max-w-[100px] px-2  items-center h-full border font-medium">
+              <div className="flex-1">
+                <IoMdRemove />
+              </div>
+              <div className="h-full flex justify-center items-center px-3">
+                {amount}
+              </div>
+              <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+                <IoMdAdd />
+              </div>
+            </div>
+            <div className="flex-1 flex justify-around items-center">
+              $ {price}
+            </div>
+            <div className="flex-1 flex justify-end items-center text-primary font-medium">{`$ ${parseFloat(
+              price * amount
+            ).toFixed(2)}`}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
